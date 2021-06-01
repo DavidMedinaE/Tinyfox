f106f8cff4e7a52bdf5a79c0701989fea110b0b2
DavidMedinaE <davidmedina96@hotmail.com>
2021-05-31 22:59:45 -0500

    Un comentario

--- Tinyfox.h
+++ Tinyfox.h
@@ -2,5 +2,6 @@
 Derechos Reservados "Teca" 2020, autor Marco A. Caballero Rev.1.5
 Contibutor: Paul Portocarrero Hernandez
 */
+//Se incluyen la libreria TiniSigfox
 #include <TinySigfox.h>
 #include <TinySigfox.cpp>
